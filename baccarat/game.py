4a070e01413be7f33259f7588a2771274c623be8
Matt Mackenzie <Matthew.Mackenzie@ibm.com>
2023-11-10 12:30:48 -0500

    remove code from actual python files

--- baccarat/game.py
+++ baccarat/game.py
@@ -45,26 +45,22 @@ class NotEnoughMoneyError(Exception):
 class BetResult(Enum):
     """A bet type."""
 
-    PLAYER = "Player"
-    BANKER = "Banker"
-    TIE = "Tie"
-
 
 class BaccaratHand:
     """A hand of cards."""
 
     def __init__(self) -> None:
-        self.cards: list[Card] = []
+        ...
 
     @property
     def total(self) -> int:
         """The total value of the hand."""
-        return self.get_value()
+        ...
 
     @property
     def num_cards(self) -> int:
         """The number of cards in the hand."""
-        return len(self.cards)
+        ...
 
     @property
     def third_card(self) -> Card | None:
@@ -72,10 +68,7 @@ class BaccaratHand:
 
         :return: The third card in the hand, or None if there is no third card
         """
-        if self.num_cards < 3:
-            return None
-
-        return self.cards[2]
+        ...
 
     @property
     def is_natural(self) -> bool:
@@ -84,7 +77,7 @@ class BaccaratHand:
 
         :return: True if the hand is a natural win, otherwise False
         """
-        return self.num_cards == 2 and self.total in [8, 9]
+        ...
 
     def add_card(self, card: Card) -> None:
         """Add a card to the hand.
@@ -101,7 +94,7 @@ class BaccaratHand:
 
         :return: The value of the hand
         """
-        return sum(get_baccarat_value(card) for card in self.cards) % 10
+        ...
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(total={self.get_value()}, cards={self.cards})"
@@ -135,11 +128,7 @@ class Player:
         :return: A bet
         """
 
-        if amount > self.bankroll:
-            raise NotEnoughMoneyError("Player does not have enough money to make bet")
-
-        self.bankroll -= amount
-        return Bet(amount, result)
+        ...
 
     def win_bet(self, amount: int) -> None:
         """Win a bet - add the amount to the bankroll.
@@ -147,7 +136,7 @@ class Player:
         :param amount: The amount won
         """
 
-        self.bankroll += amount
+        ...
 
 
 class BaccaratTable:
@@ -164,48 +153,30 @@ class BaccaratTable:
     results: list[BetResult]
 
     def __init__(self, num_decks: int = 8) -> None:
-        self.shoe = Shoe(num_decks)
-        self.shoe.shuffle()
-        logging.info(f"Shoe shuffled with {self.shoe.num_cards} cards")
-
-        self.player = None
-        self.bets = deque()
-        self.player_hand = None
-        self.banker_hand = None
-        self.results = []
+        ...
 
     @property
     def num_games(self) -> int:
         """The number of games played."""
-        return len(self.results)
+        ...
 
     @property
     def result_counts(self) -> dict[BetResult, int]:
         """The number of times each bet type has won."""
 
-        counts = Counter(self.results)
-
-        return {
-            BetResult.PLAYER: counts.get(BetResult.PLAYER, 0),
-            BetResult.BANKER: counts.get(BetResult.BANKER, 0),
-            BetResult.TIE: counts.get(BetResult.TIE, 0),
-        }
+        ...
 
     @property
     def last_result(self) -> BetResult | None:
         """The result of the last game played."""
-        if len(self.results) == 0:
-            return None
-
-        return self.results[-1]
+        ...
 
     def seat_player(self, player: Player) -> None:
         """Seat a player at the table.
 
         :param player: The player to seat
         """
-        self.player = player
-        logging.info(f"Player seated with ${player.bankroll:.02f}")
+        ...
 
     def place_bet(self, amount: int, result: BetResult) -> None:
         """Place a bet.
@@ -215,97 +186,28 @@ class BaccaratTable:
         :raises ValueError: If the bet amount is greater than the player's bankroll
         """
 
-        if self.player is None:
-            raise ValueError("Player is not set")
-
-        bet = self.player.make_bet(amount, result)
-        self.bets.append(bet)
-        logging.info(f"Player bets ${bet.amount:.02f} on '{bet.result.value}'")
+        ...
 
     def play(self) -> None:
         """Play a game of baccarat."""
-        if len(self.bets) == 0:
-            raise ValueError("No bets have been placed")
-
-        if self.shoe.num_cards < 6:
-            self.shoe.reset()
-            logging.info(f"Shoe reset with {self.shoe.num_cards} cards")
-
-        # Set up the game - deal 2 cards to the player and banker
-        logging.info(f"Starting new deal with {self.shoe.num_cards} cards")
-        self._deal()
-
-        # Play the game - draw as needed, and determine the result
-        result = self._play()
-        logging.info(f"The result is '{result.value}'")
-        self.results.append(result)
-
-        # Settle the bets - pay out winnings, if any
-        self._settle_bets(result)
-
-        if self.player is None:
-            raise ValueError("Player is not set")
-
-        logging.info(f"Player's bankroll is now ${self.player.bankroll:.02f}")
+        ...
 
     def _deal(self) -> None:
         """Deal the cards."""
 
-        self.player_hand = BaccaratHand()
-        self.banker_hand = BaccaratHand()
-
-        self.player_hand.add_card(self.shoe.deal())
-        self.banker_hand.add_card(self.shoe.deal())
-        self.player_hand.add_card(self.shoe.deal())
-        self.banker_hand.add_card(self.shoe.deal())
-
-        logging.info(f"Player has {self.player_hand}")
-        logging.info(f"Banker has {self.banker_hand}")
+        ...
 
     def _play(self) -> BetResult:
         """Play the game."""
 
-        if self.player_hand is None or self.banker_hand is None:
-            raise ValueError("Hands have not been dealt")
-
-        natural_win = check_natural(self.player_hand, self.banker_hand)
-
-        if natural_win:
-            logging.info(f"Natual! Result is '{natural_win.value}'")
-            return natural_win
-
-        do_player_draw(self.player_hand, self.shoe)
-        BaccaratTable._log_draw("Player", self.player_hand)
-
-        do_banker_draw(self.banker_hand, self.player_hand, self.shoe)
-        BaccaratTable._log_draw("Banker", self.banker_hand)
-
-        return get_result(self.player_hand, self.banker_hand)
+        ...
 
     def _settle_bets(self, result: BetResult) -> None:
-        if self.player is None:
-            raise ValueError("Player is not set")
-
-        while self.bets:
-            bet = self.bets.popleft()
-            amount = settle_bet(bet, result)
-
-            if amount == 0:
-                logging.info(f"Player loses ${bet.amount:.02f}")
-            if amount > 0:
-                logging.info(f"Player wins ${amount:.02f}")
-
-            self.player.win_bet(amount)
-
-        if len(self.bets) != 0:
-            raise ValueError("Not all bets have been settled")
+        ...
 
     @classmethod
     def _log_draw(cls, who: str, hand: BaccaratHand) -> None:
-        if hand.num_cards == 3:
-            logging.info(f"{who} draws {hand.third_card} - new total is {hand.total}")
-        else:
-            logging.info(f"{who} stands with {hand.total}")
+        ...
 
 
 def get_baccarat_value(card: Card) -> int:
@@ -315,12 +217,7 @@ def get_baccarat_value(card: Card) -> int:
     :return: The baccarat value of the card as an integer
     """
 
-    if card.value in [Value.TEN, Value.JACK, Value.QUEEN, Value.KING]:
-        return 0
-    elif card.value is Value.ACE:
-        return 1
-    else:
-        return int(card.value.value)
+    ...
 
 
 def does_player_draw(player_total: int) -> bool:
@@ -330,7 +227,7 @@ def does_player_draw(player_total: int) -> bool:
     :return: True if the player should draw a third card, otherwise False
     """
 
-    return player_total <= 5
+    ...
 
 
 def does_banker_draw(banker_total: int, player_third_card_value: int | None) -> bool:
@@ -340,16 +237,7 @@ def does_banker_draw(banker_total: int, player_third_card_value: int | None) -> 
     :param player_third_card_value: The value of the player's third card, if any
     :return: True if the banker should draw a third card, otherwise False
     """
-    if player_third_card_value is None:
-        return banker_total <= 5
-
-    return (
-        (banker_total <= 2)
-        or (banker_total == 3 and player_third_card_value != 8)
-        or (banker_total == 4 and player_third_card_value in range(2, 7 + 1))
-        or (banker_total == 5 and player_third_card_value in range(4, 7 + 1))
-        or (banker_total == 6 and player_third_card_value in range(6, 7 + 1))
-    )
+    ...
 
 
 def do_player_draw(player_hand: BaccaratHand, shoe: Shoe) -> None:
@@ -360,8 +248,7 @@ def do_player_draw(player_hand: BaccaratHand, shoe: Shoe) -> None:
     :return: The player's hand
     """
 
-    if does_player_draw(player_hand.get_value()):
-        player_hand.add_card(shoe.deal())
+    ...
 
 
 def do_banker_draw(banker_hand: BaccaratHand, player_hand: BaccaratHand, shoe: Shoe) -> None:
@@ -373,13 +260,7 @@ def do_banker_draw(banker_hand: BaccaratHand, player_hand: BaccaratHand, shoe: S
     :return: The banker's hand
     """
 
-    if player_hand.third_card is None:
-        player_third_card_value: int | None = None
-    else:
-        player_third_card_value = get_baccarat_value(player_hand.third_card)
-
-    if does_banker_draw(banker_hand.get_value(), player_third_card_value):
-        banker_hand.add_card(shoe.deal())
+    ...
 
 
 def check_natural(player_hand: BaccaratHand, banker_hand: BaccaratHand) -> BetResult | None:
@@ -390,14 +271,7 @@ def check_natural(player_hand: BaccaratHand, banker_hand: BaccaratHand) -> BetRe
     :return: The bet type if a natural win, otherwise False
     """
 
-    if player_hand.is_natural and banker_hand.is_natural:
-        return BetResult.TIE
-    elif player_hand.is_natural:
-        return BetResult.PLAYER
-    elif banker_hand.is_natural:
-        return BetResult.BANKER
-    else:
-        return None
+    ...
 
 
 def get_result(player_hand: BaccaratHand, banker_hand: BaccaratHand) -> BetResult:
@@ -408,12 +282,7 @@ def get_result(player_hand: BaccaratHand, banker_hand: BaccaratHand) -> BetResul
     :return: The result of the game
     """
 
-    if player_hand.total > banker_hand.total:
-        return BetResult.PLAYER
-    elif player_hand.total < banker_hand.total:
-        return BetResult.BANKER
-    else:
-        return BetResult.TIE
+    ...
 
 
 def settle_bet(bet: Bet, result: BetResult) -> int:
@@ -423,11 +292,4 @@ def settle_bet(bet: Bet, result: BetResult) -> int:
     :param result: the result of the game
     :return: the amount to pay out (0 if the bet loses)
     """
-    if result is BetResult.PLAYER and bet.result == result:
-        return bet.amount * 2
-    elif result is BetResult.BANKER and bet.result == result:
-        return math.floor(bet.amount * 1.95)  # 5% commission
-    elif bet.result == BetResult.TIE and result == BetResult.TIE:
-        return bet.amount * 8
-    else:
-        return 0
+    ...
--- baccarat/utils.py
+++ baccarat/utils.py
@@ -7,28 +7,13 @@ from typing import NamedTuple
 class Suit(Enum):
     """A playing card suit."""
 
-    SPADES = "♠"
-    HEARTS = "♥"
-    DIAMONDS = "♦"
-    CLUBS = "♣"
+    ...
 
 
 class Value(Enum):
     """A playing card value."""
 
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9
-    TEN = 10
-    JACK = "J"
-    QUEEN = "Q"
-    KING = "K"
-    ACE = "A"
+    ...
 
 
 class Card(NamedTuple):
@@ -45,7 +30,7 @@ class Deck:
     """A deck of cards."""
 
     def __init__(self) -> None:
-        self.cards = [Card(value, suit) for value, suit in product(Value, Suit)]
+        ...
 
 
 class Shoe:
@@ -58,61 +43,35 @@ class Shoe:
     discards: list[Card]
 
     def __init__(self, decks: int = 8) -> None:
-        self._decks = decks
-
-        self.cards = []
-        self.discards = []
-
-        for _ in range(decks):
-            self.cards.extend(Deck().cards)
+        ...
 
     @property
     def num_decks(self) -> int:
         """The number of decks in the shoe."""
-        return self._decks
+        ...
 
     @property
     def num_cards(self) -> int:
         """The number of cards remaining in the shoe."""
-        return len(self.cards)
+        ...
 
     def shuffle(self) -> None:
         """Shuffle the shoe."""
-        random.shuffle(self.cards)
+        ...
 
     def deal(self) -> Card:
         """Deal a card from the shoe.
 
         :return: A card
         """
-        card = self.cards.pop()
-        self.discards.append(card)
-
-        return card
+        ...
 
     def reset(self) -> None:
         """Reset the shoe."""
-        self.cards.extend(self.discards)
-        self.discards = []
-        self.shuffle()
+        ...
 
 
 def create_card(value: int | str, suit: str) -> Card:
     """Create a card from primitive types."""
 
-    if len(suit) > 1:
-        if suit.lower() == "spades":
-            suit = "♠"
-        elif suit.lower() == "hearts":
-            suit = "♥"
-        elif suit.lower() == "diamonds":
-            suit = "♦"
-        elif suit.lower() == "clubs":
-            suit = "♣"
-        else:
-            raise ValueError("Invalid suit")
-
-    value_ = Value(value)
-    suit_ = Suit(suit)
-
-    return Card(value_, suit_)
+    ...
